5c5c555086071d143b1c6f1c5a5b4d135aa76a08
Beau00 <70371863+Beau00@users.noreply.github.com>
2024-03-06 16:47:51 +0100

    Added animation for money add and updated money shader

--- Speakeasy Manager/Assets/Scripts/Managers/EconomyManager.cs
+++ Speakeasy Manager/Assets/Scripts/Managers/EconomyManager.cs
@@ -10,8 +10,8 @@ public class EconomyManager : MonoBehaviour
     [SerializeField] private float money;
     [SerializeField] private Text moneyText;
     [SerializeField] private Text moneyChangeText;
-    
-    
+    [SerializeField] private Animator moneyChangeAnimator;
+
 
     private void Awake()
     {
@@ -33,12 +33,14 @@ public class EconomyManager : MonoBehaviour
 
     public void IncreaseMoney(float amount)
     {
+        moneyChangeAnimator.SetTrigger("MoneyAdd");
         money += amount;
         moneyText.text = money.ToString();
         moneyChangeText.text = "+" + amount.ToString("F2");
     }
     public void DecreaseMoney(float _amount)
     {
+        moneyChangeAnimator.SetTrigger("MoneyRemove");
         money -= _amount;
         moneyText.text = money.ToString();
         moneyChangeText.text = "-" + _amount.ToString("F2");
